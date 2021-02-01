987798e616cf7e013a2ecf5071812b0fc16ee562
sarwar_sikder <amin.se.aiub@gmail.com>
2021-02-01 13:41:17 +0600

    OOP PHP basic

---
+++ abc.php
@@ -0,0 +1,47 @@
+<?php
+	
+
+	/*$name = "alamin";
+
+	$student = ['1', 'name', 'cgpa'];
+	echo $student[0];
+	//$student = array();
+
+	$student = ['id'=>'1', 'name'=>'alamin', 'cgpa'=>2.4];
+	echo $student['id'];
+
+	$students = [
+					's1'=>['id'=>'1', 'name'=>'alamin', 'cgpa'=>2.4],
+					's2'=>['id'=>'1', 'name'=>'alamin', 'cgpa'=>2.4],
+					's3'=>['id'=>'1', 'name'=>'alamin', 'cgpa'=>2.4]
+				];
+	echo $students['s1']['name'];*/
+
+	/*function sum($a=0, $b=0){
+
+		return $a+$b;
+	}
+
+	sum();*/
+
+
+	/*class Student 
+	{
+		private $name;
+		private $id;
+		private $cgpa;
+		
+		function __construct($name="", $id="", $cgpa="")
+		{
+			$this->name = $name;
+			$this->id = $id;
+			$this->cgpa = $cgpa;
+		}
+
+		public function getName(){
+			return $this->name;
+		}
+	}
+
+
+	$s1 = new Student('alamin', '12', 3);*/
